b1c5c6c8bd96e66b88f352c1df14a53294ed0967
louieiply <louieiply@gmail.com>
2022-04-11 01:36:07 +0930

    implemented past ,current, future color scheme on body.

--- Develop/script.js
+++ Develop/script.js
@@ -2,19 +2,18 @@
 
     let timeblocks_container = $("#timeblocks-container");
     const border_setting = "1px dashed black";
-    let currentDate = new Date();
-
-    
+    let currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    console.log();
 
     let timeBlock = $("<div/>").attr("class","row justify-content-center align-items-center h-100");    
     timeBlock.appendTo(timeblocks_container);
     for (let index = 8; index < 19; index++) {
-
+        
         let blockBody = $("<div/>");
         let blockTime = $("<div/>");
         let blockLink = $("<div/>");
-        let timeText = index < 12 ? $("<h5/>").text(index + "Am") : $("<h5/>").text(index + "Pm");
-        let description = $("<p/>").text("Heading. " + index);
+        let timeText = index < 12 ? $("<h5/>").text(index + " Am") : $("<h5/>").text(index + " Pm");
+        let description = $("<p/>").text(" ").attr("id",`save${index}`).attr("class","description");
         let iconContainer = $("<h5/>");
         let link = $("<a/>").attr("href","#").attr("id",`link${index}`).attr("class","link");
         let saveIcon = $("<i/>").attr("class","fas fa-save"); 
@@ -24,7 +23,17 @@
         blockTime.appendTo(timeBlock);
 
         blockBody.attr("class","col col-md-10 col-sm-10 col-lg-10 fix-height");
-        blockBody.css("background-color","blue");
+        // blockBody.css("background-color","blue");
+        if(moment().format('H') == index){
+            blockBody.addClass("current");
+        }
+        else if(moment().format('H') > index){
+            blockBody.addClass("future"); 
+        }
+        else{
+            blockBody.addClass("past"); 
+        }
+
         blockBody.appendTo(timeBlock);
 
         blockLink.attr("class","col col-md-1 col-sm-1 col-lg-1 fix-height saveBtn");
